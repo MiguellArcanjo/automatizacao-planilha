c7e080c3a7ae0f7569ce3fb5b3daeb33ae14cd33
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2024-02-10 09:08:01 -0300

    Finalizaão tela inicial

---
+++ front/src/app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import style from "@/src/styles/home.module.css";
+import "@/src/styles/global.css";
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={style.main} style={{margin: 0}}>
+      <section>
+        <h1 className={style.title}>O que gostaria de fazer?</h1>
+
+        <div>
+          <Link href="www.google.com" className={style.link}>Lançar proposta</Link>
+          <Link href="www.google.com" className={style.link}>Consultar proposta</Link>
+        </div>
+      </section>
+    </div>
+  );
+}
